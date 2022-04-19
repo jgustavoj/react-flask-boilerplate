1cadc1d0e379121e97f5f5339b1ecea0cd37566d
jgustavoj <gjime011@fiu.edu>
2022-04-19 17:47:33 -0400

    changes

--- src/api/utils.py
+++ src/api/utils.py
@@ -1,4 +1,5 @@
 from flask import jsonify, url_for
+import datetime
 
 class APIException(Exception):
     status_code = 400
@@ -34,8 +35,10 @@ def generate_sitemap(app):
     return """
         <div style="text-align: center;">
         <img style="max-height: 80px" src='https://github.com/4GeeksAcademy/react-flask-hello/blob/4677c732f09717c85156fbd71c147f0d98fcac6f/docs/assets/rigo-baby.jpg?raw=true' />
-        <h1>Rigo welcomes you to your API!!</h1>
+        <h1>Welcome to your API</h1>
         <p>API HOST: <script>document.write('<input style="padding: 5px; width: 300px" type="text" value="'+window.location.href+'" />');</script></p>
-        <p>Start working on your project by following the <a href="https://start.4geeksacademy.com/starters/full-stack" target="_blank">Quick Start</a></p>
-        <p>Remember to specify a real endpoint path like: </p>
         <ul style="text-align: left;">"""+links_html+"</ul></div>"
+
+def utcnow():
+    """Return a timezone-aware datetime that represents the current time in UTC"""
+    return datetime.datetime.now().astimezone(tz=datetime.timezone.utc) 
